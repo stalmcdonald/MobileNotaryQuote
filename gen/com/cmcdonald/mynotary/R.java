f3b03c235e00f1cc5353b2429169276ec5390ba1
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-03-06 10:22:28 -0800

    Layout Responsive/Created few Strings
    Changed activity_main.xml changed from relative to linear layout.
    created some strings for numQuote.xml

--- gen/com/cmcdonald/mynotary/R.java
+++ gen/com/cmcdonald/mynotary/R.java
@@ -14,16 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Note=0x7f070000;
-        public static final int browseBttn=0x7f070008;
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int button3=0x7f070004;
-        public static final int discountTable=0x7f070007;
-        public static final int editText1=0x7f070005;
-        public static final int menu_settings=0x7f070009;
-        public static final int quoteTable=0x7f070006;
-        public static final int textView1=0x7f070003;
+        public static final int LinearLayout1=0x7f070000;
+        public static final int Note=0x7f070001;
+        public static final int browseBttn=0x7f070009;
+        public static final int button1=0x7f070002;
+        public static final int button2=0x7f070003;
+        public static final int button3=0x7f070005;
+        public static final int discountTable=0x7f070008;
+        public static final int editText1=0x7f070006;
+        public static final int menu_settings=0x7f07000a;
+        public static final int quoteTable=0x7f070007;
+        public static final int textView1=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,15 +36,17 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int c=0x7f040007;
-        public static final int discount=0x7f040008;
+        public static final int c=0x7f040009;
+        public static final int discount=0x7f04000a;
+        public static final int etHint=0x7f040007;
         public static final int hello_world=0x7f040001;
         public static final int intro_title=0x7f040003;
         public static final int menu_settings=0x7f040002;
         public static final int num_doc=0x7f040004;
         public static final int ppp=0x7f040005;
-        public static final int q=0x7f040006;
-        public static final int quote_table=0x7f040009;
+        public static final int q=0x7f040008;
+        public static final int quote_table=0x7f04000b;
+        public static final int webBrowserBttn=0x7f040006;
     }
     public static final class style {
         /** 
--- src/com/cmcdonald/mynotary/MainActivity.java
+++ src/com/cmcdonald/mynotary/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.practice);
+		setContentView(R.layout.activity_main);
 		documents = (Button) findViewById(R.id.button1);
 		costPerPage = (Button) findViewById(R.id.button2);
 		calc = (Button) findViewById(R.id.button3);
